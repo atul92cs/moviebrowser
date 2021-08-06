073c67a1151ac94287278def8edc99315c904a24
Atul kumar singh <atul.11192@gmail.com>
2021-08-06 15:11:44 +0530

    adding db credentials

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 let mysql=require('mysql');
 let db=mysql.createConnection({
-    host:'localhost',
-    user:'root',
+    host:'bqateyin3z4fcjjgix8x-mysql.services.clever-cloud.com',
+    user:'u78zhrz26qzbx3qh',
     port:3306,
     database:'bqateyin3z4fcjjgix8x',
-    password:'seed',
+    password:'0qVcqFrNpQpClC0rhA69',
     multipleStatements:true
 });
 db.connect(err=>{
