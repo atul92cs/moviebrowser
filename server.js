7f6df700f188b6808bedd360e891e8fb83b40bdd
Atul kumar singh <atul.11192@gmail.com>
2021-08-06 15:27:51 +0530

    adding cors

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ let port=process.env.PORT||8080;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    );
+    next();
+  });
 app.use('/',movies);
 app.use('/genre',genre);
 app.use('/director',director);
