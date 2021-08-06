1b51586f3d25681ac338bc9c1d2325cf2eb60eb1
Atul kumar singh <atul.11192@gmail.com>
2021-08-06 14:43:53 +0530

    adding more code

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 let movies=require('./controllers/movie');
 let genre=require('./controllers/genre');
 let director=require('./controllers/director');
+let cors=require('cors');
 let express=require('express');
 let app=express();
 let port=process.env.PORT||8080;
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use('/',movies);
